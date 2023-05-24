daee0a449cb250a7f04d4d50c2cf9b7aaac4cbff
Mateus Andreatta <mateus-henrique2001@hotmail.com>
2023-05-24 15:01:40 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Mateus Andreatta on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
