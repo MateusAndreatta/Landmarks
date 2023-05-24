4cda1db4702427ba687c4f279b9f75a4d6713e99
Mateus Andreatta <mateus-henrique2001@hotmail.com>
2023-05-24 17:09:41 -0300

    Building Lists and Navigation

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,36 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Mateus Andreatta on 24/05/23.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable, Identifiable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    private var coordinates: Coordinates
+    
+    var image: Image {
+        Image(imageName)
+    }
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
