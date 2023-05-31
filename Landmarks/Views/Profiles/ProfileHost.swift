36791968377d290c36402226884690ff30894e77
Mateus Andreatta <mateus-henrique2001@hotmail.com>
2023-05-30 21:33:49 -0300

    Working with UI Controls

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,43 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Mateus Andreatta on 30/05/23.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @EnvironmentObject var modelData: ModelData
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            if editMode?.wrappedValue == .active {
+                Button("Cancel", role: .cancel) {
+                    draftProfile = modelData.profile
+                    editMode?.animation().wrappedValue = .inactive
+                }
+            }
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                ProfileEditor(profile: $draftProfile)
+                    .onAppear {
+                        draftProfile = modelData.profile
+                    }
+                    .onDisappear {
+                        modelData.profile = draftProfile
+                    }
+            }
+        }.padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+            .environmentObject(ModelData())
+    }
+}
